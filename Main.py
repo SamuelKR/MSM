a9f6db488f9420e29efa21eb7edf6184302d8f7c
SamuelKR <kulandairaj@hotmail.com>
2017-10-06 22:04:06 +0530

    GUI updated for sub menus

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import sqlite3, datetime, os, sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from mainwindow import Ui_mainWindow
 
 
@@ -8,9 +8,11 @@ class MyWindow(QtWidgets.QMainWindow):
         super(MyWindow, self).__init__()
         self.ui = Ui_mainWindow()
         self.ui.setupUi(self)
+        create_db()
+        self.ui.create_new.clicked.connect(lambda: self.ui.mps_sub_page.setCurrentIndex(int(0)))
         self.ui.mps_openfile_button.clicked.connect(lambda: insert_image(self))
         self.ui.mps_create_button.clicked.connect(lambda: input_validator(self))
-        self.ui.mps_clear_button.clicked.connect(lambda: clear_menu(self))
+        self.ui.mps_cancel_button.clicked.connect(lambda: cancel_menu(self))
         self.ui.mps_btn_refresh.clicked.connect(lambda: load_data(self))
         self.ui.actionMPS_Misc_Pipe_Supports.triggered.connect(lambda: self.ui.stackedWidget.setCurrentIndex(int(1)))
         self.ui.actionMES_Misc_Electrical_Supports.triggered.connect(
@@ -19,15 +21,93 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.main_menu_mes.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(int(2)))
         self.ui.mes_display_support.horizontalHeader().setVisible(True)
         self.ui.mps_display_support.horizontalHeader().setVisible(True)
+        self.ui.mps_display_support.setSelectionBehavior(self.ui.mps_display_support.SelectRows)
+        self.ui.mps_display_support.setEditTriggers(self.ui.mps_display_support.NoEditTriggers)
         load_data(self)
+
+
+def create_db():
+    conn = sqlite3.connect('msm.db')
+    c = conn.cursor()
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS MPS (
+        SLNo integer PRIMARY KEY AUTOINCREMENT,
+        SuppNo TEXT,
+        Unit TEXT,
+        Area TEXT,
+        CWP TEXT,
+        Description TEXT,
+        SuppStatus TEXT,
+        RequestedBy TEXT,
+        RequestedDate TEXT,
+        ModifiedBy TEXT,
+        ModifiedDate TEXT,
+        ImplementedBy TEXT,
+        ImplementedDate TEXT,
+        ApprovedBy TEXT,
+        ApprovedDate TEXT,
+        RejectedBy TEXT,
+        RejectedDate TEXT,
+        RejectionReason TEXT,
+        SnapShot BLOB
+    );''')
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS MES (
+        SLNo integer PRIMARY KEY AUTOINCREMENT,
+        SuppNo TEXT,
+        Unit TEXT,
+        Area TEXT,
+        CWP TEXT,
+        Description TEXT,
+        SuppStatus TEXT,
+        RequestedBy TEXT,
+        RequestedDate TEXT,
+        ModifiedBy TEXT,
+        ModifiedDate TEXT,
+        ImplementedBy TEXT,
+        ImplementedDate TEXT,
+        ApprovedBy TEXT,
+        ApprovedDate TEXT,
+        RejectedBy TEXT,
+        RejectedDate TEXT,
+        RejectionReason TEXT,
+        SnapShot BLOB
+    );''')
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS STRUC_USERS (
+        SLNo integer PRIMARY KEY AUTOINCREMENT,
+        Name TEXT,
+        Role TEXT
+    );''')
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS PIPING_USERS (
+        SLNo integer PRIMARY KEY AUTOINCREMENT,
+        Name TEXT,
+        Role TEXT
+    );''')
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS ELEC_USERS (
+        SLNo integer PRIMARY KEY AUTOINCREMENT,
+        Name TEXT,
+        Role TEXT
+    );''')
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS PROJ_INFO (
+        SLNo integer PRIMARY KEY AUTOINCREMENT,
+        ProjectName TEXT,
+        ProjectCode TEXT
+    );''')
+    conn.commit()
+    c.close()
+    conn.close()
 
 
 def load_data(self):
     conn = sqlite3.connect('msm.db')
     query = """SELECT SuppNo, Unit, Area, CWP, Description,
             SuppStatus, RequestedBy, RequestedDate, ModifiedBy,
-            ModifiedDate, Implementedby, ImplementedDate, AcceptedBy,
-            AcceptedDate, RejectedBy, RejectedDate FROM MPS"""
+            ModifiedDate, ImplementedBy, ImplementedDate, ApprovedBy,
+            ApprovedDate, RejectedBy, RejectedDate, RejectionReason FROM MPS"""
     result = conn.execute(query)
     self.ui.mps_display_support.setRowCount(0)
     for row_number, row_data in enumerate(result):
@@ -44,7 +124,7 @@ def insert_image(self):
     file_name.close()
 
 
-def clear_menu(self):
+def cancel_menu(self):
     self.ui.mps_unit_line_edit.clear()
     self.ui.mps_area_line_edit.clear()
     self.ui.mps_cwp_line_edit.clear()
@@ -52,6 +132,7 @@ def clear_menu(self):
     file_name = open("filename", 'w')
     file_name.write(" ")
     file_name.close()
+    self.ui.mps_cancel_button.clicked.connect(lambda: self.ui.mps_sub_page.setCurrentIndex(int(1)))
 
 
 def time_stamp():
@@ -93,18 +174,18 @@ def create_supp(self):
     c = conn.cursor()
     c.execute("""INSERT INTO MPS
               (Unit, Area, CWP, Description, SuppStatus, RequestedBy, RequestedDate, SnapShot, ModifiedBy,
-              ModifiedDate, Implementedby, ImplementedDate, AcceptedBy, AcceptedDate, RejectedBy, RejectedDate)
-               VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);""", (
-        _unit, _area, _cwp, _desc, "Created", os.getlogin(), time_stamp(), _imagename, "", "", "", "", "", "", "", ""))
+              ModifiedDate, ImplementedBy, ImplementedDate, ApprovedBy, ApprovedDate, RejectedBy, RejectedDate, RejectionReason)
+               VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);""", (
+        _unit, _area, _cwp, _desc, "Created", os.getlogin(), time_stamp(), _imagename, "", "", "", "", "", "", "", "",""))
     pr = (c.lastrowid)
     c.execute("UPDATE MPS SET SuppNo=? WHERE SLNo=?", ["MPS - " + str(pr), pr])
     conn.commit()
     QtWidgets.QMessageBox.information(w, "Message", "New MPS" + str(pr) + "Created")
     c.close()
     conn.close()
-    QtWidgets.QMessageBox.information(w, "Message", "New Support Created")
+    QtWidgets.QMessageBox.question(w, "Message", "Do you want to send notification?")
     load_data(self)
-    clear_menu(self)
+    cancel_menu(self)
 
 
 if __name__ == '__main__':
